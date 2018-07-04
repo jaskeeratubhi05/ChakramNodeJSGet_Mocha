15fb0b0a7e64d109e9c8b1c1c9c9b29ffdc2dac0
lkkushan101 <kam@codehousegroup.com>
2018-07-04 09:05:58 +0530

    v2

--- chakram.js
+++
@@ -1,7 +0,0 @@
-var chakram = require('C:/slimerTest/node_modules/chakram');
-
-describe("Chakram", function() {
-    it("should offer simple HTTP request capabilities", function () {
-        return chakram.get("https://reqres.in/api/users/2");
-    });
-});
